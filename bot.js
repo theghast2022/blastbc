7a695997c0f58ba74fc16ec287c7cc9fba53f40c
theghast2022 <44522450+theghast2022@users.noreply.github.com>
2019-01-13 19:36:33 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,17 +14,52 @@ client.on('ready', () => {
   console.log(`Welcome Bro ${client.user.tag}!`);
 });
 
+client.on('message', message => {
+  if (!message.content.startsWith(prefix)) return;
+  var args = message.content.split(' ').slice(1);
+  var argresult = args.join(' ');
+  if (message.author.id !== "508574921010577409") return;
 
-client.on('ready', () => {
-   console.log(`----------------`);
-      console.log(`Desert Bot- Code By : ZorexHost`);
-        console.log(`----------------`);
-      console.log(`ON ${client.guilds.size} Servers '     Code By : ZorexHost `);
-    console.log(`----------------`);
-  console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`$help | in ${client.guilds.size} Servers `,"http://twitch.tv/S-F")
-client.user.setStatus("dnd")
-}); 
+  
+  if (message.content.startsWith(prefix + 'wt')) {
+  client.user.setActivity(argresult, {type: 'WATCHING'})
+     console.log('test' + argresult);
+    message.channel.sendMessage(`Watch Now: **${argresult}**`)
+} 
+
+ 
+  if (message.content.startsWith(prefix + 'ls')) {
+  client.user.setActivity(argresult, {type: 'LISTENING'})
+     console.log('test' + argresult);
+    message.channel.sendMessage(`LISTENING Now: **${argresult}**`)
+} 
+
+
+if (message.content.startsWith(prefix + 'setname')) {
+  client.user.setUsername(argresult).then
+      message.channel.sendMessage(`Username Changed To **${argresult}**`)
+  return message.reply("You Can change the username 2 times per hour");
+} 
+
+if (message.content.startsWith(prefix + 'setavatar')) {
+  client.user.setAvatar(argresult);
+   message.channel.sendMessage(`Avatar Changed Successfully To **${argresult}**`);
+}
+
+if (message.content.startsWith(prefix + 'st')) {
+  client.user.setGame(argresult, "https://www.twitch.tv/peery13");
+     console.log('test' + argresult);
+    message.channel.sendMessage(`Streaming: **${argresult}**`)
+} 
+if (message.content.startsWith(prefix + 'ply')) {
+  client.user.setGame(argresult);
+     console.log('test' + argresult);
+    message.channel.sendMessage(`Playing: **${argresult}**`)
+} 
+
+
+
+});
 
 client.on('message',async message => {
 if(message.author.bot || message.channel.type === 'dm') return;
